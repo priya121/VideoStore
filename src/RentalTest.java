113dc49d8ff2dc2ce92d7a5b5f1cce561a30e5d8
priya121 <priya121@github.com>
2016-02-15 19:06:56 +0000

    Adds tests for Rental class.

--- src/RentalTest.java
+++ src/RentalTest.java
@@ -1,3 +1,18 @@
-public class RentalTest {
+import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
+public class RentalTest {
+    Movie officeSpace = new Movie("Office Space", 5);
+    Rental twoDayRental = new Rental(officeSpace, 2);
+
+    @Test
+    public void checksMovieRented() {
+        assertEquals(officeSpace, twoDayRental.getMovie());
+    }
+
+    @Test
+    public void checksNumberOfDaysRented() {
+        assertEquals(2, twoDayRental.getDaysRented());
+    }
 }
