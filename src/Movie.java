4027e95b520c33ba159d7c98350df1bfc7040554
priya121 <priya121@github.com>
2016-02-15 18:52:44 +0000

    Original VideoStore code.

---
+++ src/Movie.java
@@ -0,0 +1,27 @@
+public class Movie {
+
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+
+    private int priceCode;
+    private String title;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+
+    public void setPriceCode(int arg) {
+        priceCode = arg;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
