28e17852050d785a86b0547320533fa6ac42215d
priya121 <priya121@github.com>
2016-02-16 11:38:30 +0000

    Moved getCharge method to Movie so that movie type data is in the same class (more likely to change).

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -6,7 +6,6 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
 
-
     private int priceCode;
     private String title;
 
@@ -24,6 +23,27 @@ public class Movie {
     }
 
     public String getTitle() {
+
         return title;
+    }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
     }
 }
