6b063c3b464e0b469be1e04f9b4fa10b7632ed01
priya121 <priya121@github.com>
2016-02-24 11:49:19 +0000

    Renamed tests in Statement Class.

--- src/test/StatementTest.java
+++ src/test/StatementTest.java
@@ -22,63 +22,63 @@ public class StatementTest {
     Movie childrensMovie = new Movie("Childrens Movie", Movie.CHILDRENS);
     Movie newRelease = new Movie("New Release Movie", Movie.NEW_RELEASE);
 
-    Rental twoDayOfficeSpace = new Rental(regularMovie, 2);
-    Rental threeDayOfficeSpace = new Rental(regularMovie, 3);
-    Rental threeDayHarryPotter = new Rental(childrensMovie, 3);
-    Rental fourDayHarryPotter = new Rental(childrensMovie, 4);
-    Rental twoDayStarWars = new Rental(newRelease, 3);
+    Rental twoDayRegular = new Rental(regularMovie, 2);
+    Rental threeDayRegular = new Rental(regularMovie, 3);
+    Rental threeDayChildrens = new Rental(childrensMovie, 3);
+    Rental fourDayChildrens = new Rental(childrensMovie, 4);
+    Rental twoDayNewRelease = new Rental(newRelease, 3);
 
     @Test
-    public void createsStatementForCustomerPriyaSingleRegularRental() {
-        statement.addRental(twoDayOfficeSpace);
+    public void testsTwoDayRegularRentalTotals() {
+        statement.addRental(twoDayRegular);
         assertEquals(2.0, statement.getTotalCharge());
         assertEquals(1, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
-    public void createsStatementForCustomerPriyaLongerRegularRental() {
-        statement.addRental(threeDayOfficeSpace);
+    public void testsThreeDayRegularRentalTotals() {
+        statement.addRental(threeDayRegular);
         assertEquals(3.5, statement.getTotalCharge());
         assertEquals(1, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
-    public void createsStatementForCustomerJoMultipleRentals() {
-        statement.addRental(threeDayHarryPotter);
-        statement.addRental(twoDayOfficeSpace);
+    public void testsCombinationThreeDayChildrenAndTwoDayRegularTotals() {
+        statement.addRental(threeDayChildrens);
+        statement.addRental(twoDayRegular);
         statement.generate();
         assertEquals(3.5, statement.getTotalCharge());
         assertEquals(2, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
-    public void createsStatementForLongerChildrens() {
-        statement.addRental(fourDayHarryPotter);
+    public void testsFourDayChildrensTotals() {
+        statement.addRental(fourDayChildrens);
         assertEquals(3.0, statement.getTotalCharge());
         assertEquals(1, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
-    public void createsStatementForNewRelease() {
-        statement.addRental(twoDayStarWars);
+    public void testsTwoDayNewReleaseTotals() {
+        statement.addRental(twoDayNewRelease);
         assertEquals(9.0, statement.getTotalCharge());
         assertEquals(2, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
-    public void testMultipleRegularStatementTotals() {
-        statement.addRental(twoDayStarWars);
-        statement.addRental(fourDayHarryPotter);
-        statement.addRental(threeDayOfficeSpace);
+    public void testsMultipleRegularStatementTotals() {
+        statement.addRental(twoDayNewRelease);
+        statement.addRental(fourDayChildrens);
+        statement.addRental(threeDayRegular);
         Assert.assertEquals(15.5, statement.getTotalCharge());
         Assert.assertEquals(4, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
-    public void testMultipleRegularStatementFormat() {
-        statement.addRental(twoDayStarWars);
-        statement.addRental(fourDayHarryPotter);
-        statement.addRental(threeDayOfficeSpace);
+    public void testsMultipleRegularStatementFormat() {
+        statement.addRental(twoDayNewRelease);
+        statement.addRental(fourDayChildrens);
+        statement.addRental(threeDayRegular);
         assertEquals("Rental Record for Customer\n" +
                         "\tNew Release Movie\t9.0\n" +
                         "\tChildrens Movie\t3.0\n" +
