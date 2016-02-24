1b13ffdf8900f6a913d180a304aa3929f420a9f5
priya121 <priya121@github.com>
2016-02-24 11:32:19 +0000

    Combined multiple utomer objects in test into a single statement object representing a generic customer.

--- src/test/StatementTest.java
+++ src/test/StatementTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import junit.framework.Assert;
 import main.Statement;
 import main.Movie;
 import main.Rental;
@@ -8,11 +9,7 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class StatementTest {
-    Statement priya = new Statement("Priya");
-    Statement jo = new Statement("Jo");
-    Statement andrew = new Statement("Andrew");
-    Statement katherine = new Statement("Katherine");
-    Statement sarah = new Statement("Sarah");
+    Statement statement = new Statement("Customer");
 
     Movie officeSpace = new Movie("Office Space", Movie.REGULAR);
     Movie harryPotter = new Movie("Harry Potter", Movie.CHILDRENS);
@@ -26,57 +23,62 @@ public class StatementTest {
 
     @Test
     public void createsStatementForCustomerPriyaSingleRegularRental() {
-        priya.addRental(twoDayOfficeSpace);
-        priya.generate();
-        assertEquals(2.0, priya.getTotalCharge());
-        assertEquals(1, priya.getTotalFrequentRenterPoints());
+        statement.addRental(twoDayOfficeSpace);
+        assertEquals(2.0, statement.getTotalCharge());
+        assertEquals(1, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
     public void createsStatementForCustomerPriyaLongerRegularRental() {
-        priya.addRental(threeDayOfficeSpace);
-        priya.generate();
-        assertEquals(3.5, priya.getTotalCharge());
-        assertEquals(1, priya.getTotalFrequentRenterPoints());
+        statement.addRental(threeDayOfficeSpace);
+        assertEquals(3.5, statement.getTotalCharge());
+        assertEquals(1, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
     public void createsStatementForCustomerJoMultipleRentals() {
-        jo.addRental(threeDayHarryPotter);
-        jo.addRental(twoDayOfficeSpace);
-        jo.generate();
-        assertEquals(3.5, jo.getTotalCharge());
-        assertEquals(2, jo.getTotalFrequentRenterPoints());
+        statement.addRental(threeDayHarryPotter);
+        statement.addRental(twoDayOfficeSpace);
+        statement.generate();
+        assertEquals(3.5, statement.getTotalCharge());
+        assertEquals(2, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
     public void createsStatementForLongerChildrens() {
-        katherine.addRental(fourDayHarryPotter);
-        katherine.generate();
-        assertEquals(3.0, katherine.getTotalCharge());
-        assertEquals(1, katherine.getTotalFrequentRenterPoints());
+        statement.addRental(fourDayHarryPotter);
+        assertEquals(3.0, statement.getTotalCharge());
+        assertEquals(1, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
     public void createsStatementForNewRelease() {
-        andrew.addRental(twoDayStarWars);
-        andrew.generate();
-        assertEquals(9.0, andrew.getTotalCharge());
-        assertEquals(2, andrew.getTotalFrequentRenterPoints());
+        statement.addRental(twoDayStarWars);
+        assertEquals(9.0, statement.getTotalCharge());
+        assertEquals(2, statement.getTotalFrequentRenterPoints());
+    }
+
+    @Test
+    public void testMultipleRegularStatementTotals() {
+        statement.addRental(twoDayStarWars);
+        statement.addRental(fourDayHarryPotter);
+        statement.addRental(threeDayOfficeSpace);
+        Assert.assertEquals(15.5, statement.getTotalCharge());
+        Assert.assertEquals(4, statement.getTotalFrequentRenterPoints());
     }
 
     @Test
     public void testMultipleRegularStatementFormat() {
-        sarah.addRental(twoDayStarWars);
-        sarah.addRental(fourDayHarryPotter);
-        sarah.addRental(threeDayOfficeSpace);
+        statement.addRental(twoDayStarWars);
+        statement.addRental(fourDayHarryPotter);
+        statement.addRental(threeDayOfficeSpace);
         assertEquals("Rental Record for Sarah\n" +
                         "\tStar Wars\t9.0\n" +
                         "\tHarry Potter\t3.0\n" +
                         "\tOffice Space\t3.5\n" +
                         "Amount owed is 15.5\n" +
                         "You earned 4 frequent renter points",
-                sarah.generate());
+                statement.generate());
     }
 
 }
